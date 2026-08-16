3620f483e5efb7afa64e58d5a4f97b2207550bd3
manasirathi4 <manasirathi20@gmail.com>
2026-08-16 21:15:37 +0530

    little changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express=require("express");
 const app=express();
-app.use((req,res)=>{
-    res.send("world is beautiful!");
+//app.use((req,res)=>{
+
+    
+    //res.send("world is beautiful!");
+
+//});
+
+app.use("/hello",(req,res)=>{
+    res.send("hiiii!");
 
 });
 
+app.use("/test",(req,res)=>{
+    res.send("abracadabra!!");
 
-
+});
 
 app.listen(3000,() =>{
 
